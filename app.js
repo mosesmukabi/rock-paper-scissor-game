c66dfa90093ed091f3ed796b7f5b853034687b70
moses mukabi <mosesmukabi9@gmai.com>
2024-09-19 21:20:48 +0300

    rock, paper scissors game

---
+++ app.js
@@ -0,0 +1,74 @@
+const computers_points = document.getElementById("computerPoints");
+const players_points = document.getElementById("playersPoints");
+const computerPlayerItem = document.getElementById("currentComputerPlayer");
+const playerItem = document.getElementById("currentPlayer");
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+let computerScore = 0;
+let playerScore = 0;
+
+function play() {
+  const playerOptions = [rockBtn, paperBtn, scissorsBtn];
+
+  const computerOptions = ["👊", "✋", "✌"];
+
+  playerOptions.forEach((options) => {
+    options.addEventListener("click", function () {
+      let player = options.textContent;
+      playerItem.textContent = player;
+      const choiceNumber = Math.floor(Math.random() * 3);
+
+      const computerValue = computerOptions[choiceNumber];
+
+      computerPlayerItem.textContent = computerValue;
+
+      results(player, computerValue);
+    });
+  });
+}
+
+const results = (player, computer) => {
+  const game_results = document.getElementById("results");
+
+  player = player.toLowerCase();
+  computer = computer.toLowerCase();
+
+  if (player === computer) {
+    game_results.textContent = "Draw";
+  } else if (player == "✌") {
+    if (computer == "✋") {
+      game_results.textContent = "Player Wins";
+      playerScore++;
+      players_points.textContent = playerScore;
+    } else {
+      game_results.innerText = "Computer Wins";
+      computerScore++;
+      computers_points.textContent = computerScore;
+    }
+  } else if (player === "👊") {
+    if (computer === "✌") {
+      game_results.textContent = "Player Wins";
+      playerScore++;
+      players_points.textContent = playerScore;
+    } else {
+      game_results.textContent = "Computer Wins";
+      computerScore++;
+      computers_points.textContent = computerScore;
+    }
+  } else if (player == "✋") {
+    if (computer == "👊") {
+      game_results.textContent = "Player Wins";
+      playerScore++;
+      players_points.textContent = playerScore;
+    } else {
+      game_results.textContent = "Computer Wins";
+      computerScore++;
+      computers_points.textContent = computerScore;
+    }
+  }
+};
+
+play();
